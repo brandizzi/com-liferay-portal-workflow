d1f54c048c52c646466e3c25eaa58094003418ce
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-09-28 18:19:17 -0300

    LPS-74975 / LPS-73731 Export interfaces needed for extensibility.

--- portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/SiteAdministrationWorkflowPortlet.java
+++ portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/SiteAdministrationWorkflowPortlet.java
@@ -14,9 +14,9 @@
 
 package com.liferay.portal.workflow.web.internal.portlet;
 
+import com.liferay.portal.workflow.web.constants.WorkflowWebKeys;
 import com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys;
-import com.liferay.portal.workflow.web.internal.constants.WorkflowWebKeys;
-import com.liferay.portal.workflow.web.internal.portlet.tab.WorkflowPortletTab;
+import com.liferay.portal.workflow.web.portlet.tab.WorkflowPortletTab;
 
 import java.util.Arrays;
 import java.util.List;
