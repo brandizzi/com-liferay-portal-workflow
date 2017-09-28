09cc8db354e0e3a5c5a0d2ffad19c9ed47dfbbd5
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-09-28 14:55:02 -0300

    LPS-74975 / LPS-73731 Remove dynamic include map.

--- portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/BaseWorkflowPortlet.java
+++ portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/BaseWorkflowPortlet.java
@@ -16,7 +16,6 @@ package com.liferay.portal.workflow.web.internal.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.servlet.SessionErrors;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.workflow.WorkflowException;
 import com.liferay.portal.workflow.web.constants.WorkflowWebKeys;
@@ -25,9 +24,6 @@ import com.liferay.portal.workflow.web.portlet.tab.WorkflowPortletTab;
 import java.io.IOException;
 
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.portlet.ActionRequest;
@@ -36,45 +32,17 @@ import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
-
 /**
  * @author Adam Brandizzi
  */
 public abstract class BaseWorkflowPortlet extends MVCPortlet {
 
-	public String getDefaultTab() {
-		List<WorkflowPortletTab> portletTabs = getPortletTabs();
-
-		WorkflowPortletTab portletTab = portletTabs.get(0);
-
-		return portletTab.getName();
-	}
-
 	public abstract List<WorkflowPortletTab> getPortletTabs();
-
-	public List<String> getWorkflowTabNames() {
-		List<WorkflowPortletTab> portletTabs = getPortletTabs();
-
-		Stream<WorkflowPortletTab> stream = portletTabs.stream();
-
-		return stream.map(
-			portletTab -> portletTab.getName()
-		).collect(
-			Collectors.toList()
-		);
-	}
 
 	@Override
 	public void processAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws IOException, PortletException {
-
-		actionRequest.setAttribute(
-			WorkflowWebKeys.WORKFLOW_DEFAULT_TAB, getDefaultTab());
 
 		for (WorkflowPortletTab portletTab : getPortletTabs()) {
 			portletTab.prepareProcessAction(actionRequest, actionResponse);
@@ -124,11 +92,20 @@ public abstract class BaseWorkflowPortlet extends MVCPortlet {
 		}
 	}
 
+	protected WorkflowPortletTab getDefaultPortletTab() {
+		List<WorkflowPortletTab> portletTabs = getPortletTabs();
+
+		return portletTabs.get(0);
+	}
+
 	protected WorkflowPortletTab getSelectedPortletTab(
 		RenderRequest renderRequest) {
 
-		String tabName = ParamUtil.get(renderRequest, "tab", getDefaultTab());
+		WorkflowPortletTab defaultPortletTab = getDefaultPortletTab();
 		List<WorkflowPortletTab> portletTabs = getPortletTabs();
+
+		String tabName = ParamUtil.get(
+			renderRequest, "tab", defaultPortletTab.getName());
 
 		Stream<WorkflowPortletTab> stream = portletTabs.stream();
 
@@ -136,34 +113,8 @@ public abstract class BaseWorkflowPortlet extends MVCPortlet {
 			portletTab -> portletTab.getName().equals(tabName)
 		).findFirst(
 		).orElse(
-			portletTabs.get(0)
+			defaultPortletTab
 		);
 	}
-
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void setDynamicInclude(
-		WorkflowPortletTab dynamicInclude, Map<String, Object> properties) {
-
-		String tabsName = MapUtil.getString(
-			properties, "portal.workflow.tabs.name");
-
-		_dynamicIncludes.put(tabsName, dynamicInclude);
-	}
-
-	protected void unsetDynamicInclude(
-		WorkflowPortletTab dynamicInclude, Map<String, Object> properties) {
-
-		String tabsName = MapUtil.getString(
-			properties, "portal.workflow.tabs.name");
-
-		_dynamicIncludes.remove(tabsName);
-	}
-
-	private final Map<String, WorkflowPortletTab> _dynamicIncludes =
-		new ConcurrentHashMap<>();
 
 }
--- portal-workflow-web/src/main/resources/META-INF/resources/init.jsp
+++ portal-workflow-web/src/main/resources/META-INF/resources/init.jsp
@@ -20,8 +20,7 @@ page import="com.liferay.portal.workflow.web.internal.constants.WorkflowPortletK
 page import="com.liferay.portal.workflow.web.portlet.tab.WorkflowPortletTab" %><%@
 page import="com.liferay.taglib.servlet.PipingServletResponse" %>
 
-<%@ page import="java.util.List" %><%@
-page import="java.util.Map" %>
+<%@ page import="java.util.List" %>
 
 <%@ page import="javax.portlet.PortletURL" %>
 
