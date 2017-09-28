847b6dc73b29851260845c60e3e9e1e4ee8d4522
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-09-28 11:35:37 -0300

    LPS-74975 / LPS-73731 Make workflow portlets return a list of their portlet tabs.
    This way, we can return portlet tabs instead of tab names. Also, now we inject
    the needed tabs inside each portlet as well.

--- portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/UserWorkflowPortlet.java
+++ portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/UserWorkflowPortlet.java
@@ -16,6 +16,7 @@ package com.liferay.portal.workflow.web.internal.portlet;
 
 import com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys;
 import com.liferay.portal.workflow.web.internal.constants.WorkflowWebKeys;
+import com.liferay.portal.workflow.web.internal.portlet.tab.WorkflowPortletTab;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +24,8 @@ import java.util.List;
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Adam Brandizzi
@@ -55,8 +58,19 @@ import org.osgi.service.component.annotations.Component;
 public class UserWorkflowPortlet extends BaseWorkflowPortlet {
 
 	@Override
+	public List<WorkflowPortletTab> getPortletTabs() {
+		return Arrays.asList(mySubmissionsPortletTab);
+	}
+
+	@Override
 	public List<String> getWorkflowTabNames() {
 		return Arrays.asList(WorkflowWebKeys.WORKFLOW_TAB_MY_SUBMISSIONS);
 	}
+
+	@Reference(
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(portal.workflow.tabs.name=" + WorkflowWebKeys.WORKFLOW_TAB_MY_SUBMISSIONS + ")"
+	)
+	protected WorkflowPortletTab mySubmissionsPortletTab;
 
 }
